ce0f7bfb47882a0a7e1f7843f5d735143aaae6da
Caira Telesford <cairatelesford@Cairas-Air.fios-router.home>
2020-07-20 17:30:30 -0400

    last commit

--- app.js
+++ app.js
@@ -193,6 +193,14 @@ fetch(url)
     
 
 
+    function myFunction() {
+      var x = document.getElementById("myLinks");
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+    }
 
 
 
